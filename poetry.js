35571c5cc391902eda60f881f519f3504f2bfef1
Becky Greubel <beckygreubel@gmail.com>
2021-05-05 09:19:34 -0600

    clean up poetry, add in more of my own.

--- poetry.js
+++ poetry.js
@@ -1,7 +1,7 @@
 var paramsString = window.location; // get the current url
 var searchParams = new URLSearchParams(paramsString.search); // get the search parameters from the url
 var input = searchParams.get('input');
-var numPoems = 10;
+var numPoems = 12;
 
 let photos = ['img1.jpeg', 
     'img2.jpeg', 
@@ -17,6 +17,90 @@ let photos = ['img1.jpeg',
     'img12.jpeg', 
     'img13.jpeg'];
 
+let exampleLines = [
+        'sometimes I dream of a new place,',
+        'a place where all the things', 
+        'we hope for are true',
+        'we are there',
+        'floating,',
+        'breathing, hoping and',
+        'longing',
+        'for a new time, a new place',
+        'and yet here we stand',
+        'in our current one,', 
+        'lopsided and',
+        'saddened by the way that we',
+        'allow ourselves to destroy',
+        'all that is around us',
+        'destroying in the name of creating',
+        'a creating that is lost at heart',
+        'and overshadowed',
+        'by our own selfish gain',
+        'tomorrow we will awaken from this',
+        'terrible long slumber',
+        'and realize that we have',
+        'burnt our hopes',
+        'to the ground in the',
+        'dream of making our life',
+        'a living heaven',
+        'and yet we have made it a hell',
+        'lighting the night sky',
+        'creating the mother',
+        'disappears at dawn',
+        'death sounds like sobbing',
+        'death looks like a wilted flower',
+        'orchid',
+        'blooming and light',
+        'tropical perfection',
+        'paradise in a world of hate',
+        'lovely',
+        'in my dreams I am a ballerina',
+        'I am graceful like a swan,',
+        'twirling, leaping, soaring',
+        'beautiful and glamorous',
+        'would I be somewhere',
+        'different if I',
+        'actually had listened',
+        'to myself when I spoke',
+        'in my head about',
+        'the ways that I want to be',
+        'in this world, the shapes that I',
+        'want to manifest',
+        'and take up space in these shapes',
+        'that have rigid and soft',
+        'edges - rigid and soft',
+        'around the edges was',
+        'how I hoped to be and yet',
+        'here I am wandering in',
+        'a fog of my own creation',
+        'mapless, crying on',
+        'the path that I carved',
+        'out of the lies',
+        'I told myself in the hopes',
+        'that others would think',
+        'that I was soft and rigid',
+        'like a knife in the back',
+        'of a pillow',
+        'one day I will learn',
+        'that I must be more like myself',
+        'if I am ever going to make it',
+        'make it past my current barriers',
+        'make it through the wall',
+        'of my insecurity',
+        'insecurity that gnaws at my insides',
+        'until I am no longer whole',
+        'but instead am small pieces',
+        'that do not fit quite together',
+        'you are here',
+        'you are near',
+        'you are staying close',
+        'you are close to what you feel',
+        'you feel like flying',
+        'you fly away',
+        'you are away',
+        'away from what you love',
+        'because you do not feel love'];
+
 let last_b;
 let synonyms = [];
 let verbs = [];
@@ -30,7 +114,7 @@ findSynonyms(last_a).then(function(){
 
     todaysDate();
     for (let i = 0; i < numPoems; i++){
-            let poem = makePoem(); // select from an array of diff types of poems
+            let poem = makePoem(); 
             let photo = addPhoto();
 
             let img = document.createElement('img');
@@ -44,7 +128,12 @@ findSynonyms(last_a).then(function(){
             let div = document.createElement('div');
             div.classList.add('poem');
             poem.forEach(function(line){
-                div.innerHTML += line;
+                div.innerHTML += line + ' ';
+                let randomNumBreaks = Math.floor(Math.random() * 5) + 1;
+                for (let j = 0; j < randomNumBreaks; j++){
+                    div.innerHTML += '<br>';
+                }
+
             })
 
             document.getElementById('poemsHere').appendChild(img);
@@ -107,7 +196,6 @@ findSynonyms(last_a).then(function(){
                   Bindery.RunningHeader({
                       // render is a parameter to running header
                         render: (pageInfo, pages) => {
-                            console.log(pages);
                             if (pageInfo.number === 1){
                                 return ` `;
                             } else {
@@ -141,7 +229,7 @@ function makePoem(){
     let numLines = Math.floor(Math.random() * maxNumLines) + 5;
     let lines = [];
     for (let i = 0; i < numLines; i++){
-        lines.push(createLine() + "<br>");
+        lines.push(createLine());
     }
     return lines;
 }
@@ -151,72 +239,94 @@ function createLine(){
     let randomPOS = Math.floor(Math.random() * linesPOS.length); 
     let lineStructure = linesPOS[randomPOS];
     let word;
-    for (let k = 0; k < lineStructure.length; k++){
-        if (lineStructure[k]=='to'){
-            word = 'to';
-        } else if (lineStructure[k] == 'in'){
-            let possiblePreps = ['for', 'on', 'in', 'at', 'of', 'by'];
-            let index = Math.floor(Math.random() * possiblePreps.length);
-            word = possiblePreps[index];
-        } else if (/nn.*/.test(lineStructure[k])){
-            if (lineStructure[k] == 'nns'){
-                let plural = RiTa.pluralize(chooseSynonym())
-                if (plural){
-                    word = plural
+    let line;
+    if (Math.random() < .5){
+        for (let k = 0; k < lineStructure.length; k++){
+            if (lineStructure[k]=='to'){
+                word = 'to';
+            } else if (lineStructure[k] == 'in'){
+                let possiblePreps = ['for', 'on', 'in', 'at', 'of', 'by'];
+                let index = Math.floor(Math.random() * possiblePreps.length);
+                word = possiblePreps[index];
+            } else if (/nn.*/.test(lineStructure[k])){
+                if (lineStructure[k] == 'nns'){
+                    let plural = RiTa.pluralize(chooseSynonym())
+                    if (plural){
+                        word = plural
+                    } else {
+                        chooseSynonym();
+                    }
                 } else {
-                    chooseSynonym();
+                    let newSyn = chooseSynonym();
+                    if (newSyn){
+                        word = newSyn
+                    } else {
+                        word = chooseNoun();
+                    }
                 }
-            } else {
-                let newSyn = chooseSynonym();
-                if (newSyn){
-                    word = newSyn
+             } else if (/vb.*/.test(lineStructure[k])) {
+                word = chooseVerb();
+            } else if (lineStructure[k] == 'ex') {
+                word = 'there';
+            } else if (lineStructure[k] == 'dt'){
+                word = 'the';
+            } else if (lineStructure[k] == 'cd'){
+                word = 'one';
+            } else if (lineStructure[k] == 'cc'){
+                let ccs = ['and', 'for', 'but', 'and', 'nor', 'and', 'or', 'so', 'and', 'so']
+                let index = Math.floor(Math.random() * ccs.length);
+                word = ccs[index];
+            } else if (lineStructure[k] == 'md'){
+                let modal = ['can', 'could', 'may', 'should', 'must', 'might', 'may', 'could', 'should'];
+                let index = Math.floor(Math.random() * modal.length);
+                word = modal[index];
+            } else if (lineStructure[k] == 'wrd'){
+                let whWord = ['who', 'what', 'when', 'where', 'how', 'why'];
+                let index = Math.floor(Math.random() * whWord.length);
+                word = whWord[index];
+            } else if (/prp.*/.test(lineStructure[k])){
+                if (lineStructure[k] == 'prp$'){
+                    let possessive = ['mine', 'yours', 'his', 'hers', 'theirs', 'ours'];
+                    let index = Math.floor(Math.random() * possessive.length);
+                    word = possessive[index];
                 } else {
-                    word = chooseNoun();
+                    let possibleNouns = ['I', 'you', 'he', 'we', 'her', 'you', 'you', 'we', 'you']
+                    let index = Math.floor(Math.random() * possibleNouns.length);
+                    word = possibleNouns[index];
+                }
+            } else if (lineStructure[k] == 'jj') {
+                word = chooseNoun();
+            } else if (/rb.*/.test(lineStructure[k])){
+                word = chooseAdverb();
+            } else {
+                word = ''
+            }
+            if (k > 0 && word == lineByToken[k-1]){
+                word = ''
+            }
+            if (Math.random() <.2){
+                lineByToken.push('&nbsp;');
+                if (Math.random() < .4){
+                    lineByToken.push('&nbsp;');
+                    if (Math.random() < .3){
+                        lineByToken.push('&nbsp;');
+                    }
                 }
             }
-         } else if (/vb.*/.test(lineStructure[k])) {
-            word = chooseVerb();
-        } else if (lineStructure[k] == 'ex') {
-            word = 'there';
-        } else if (lineStructure[k] == 'dt'){
-            word = 'the';
-        } else if (lineStructure[k] == 'cd'){
-            word = 'one';
-        } else if (lineStructure[k] == 'cc'){
-            let ccs = ['and', 'for', 'but', 'and', 'nor', 'and', 'or', 'yet', 'so', 'and', 'so']
-            let index = Math.floor(Math.random() * ccs.length);
-            word = ccs[index];
-        } else if (lineStructure[k] == 'md'){
-            let modal = ['can', 'could', 'may', 'should', 'must', 'might', 'may', 'could', 'should'];
-            let index = Math.floor(Math.random() * modal.length);
-            word = modal[index];
-        } else if (lineStructure[k] == 'wrd'){
-            let whWord = ['who', 'what', 'when', 'where', 'how', 'why'];
-            let index = Math.floor(Math.random() * whWord.length);
-            word = whWord[index];
-        } else if (/prp.*/.test(lineStructure[k])){
-            if (lineStructure[k] == 'prp$'){
-                let possessive = ['mine', 'yours', 'his', 'hers', 'theirs', 'ours'];
-                let index = Math.floor(Math.random() * possessive.length);
-                word = possessive[index];
-            } else {
-                let possibleNouns = ['I', 'you', 'he', 'we', 'her', 'you', 'you', 'we', 'you']
-                let index = Math.floor(Math.random() * possibleNouns.length);
-                word = possibleNouns[index];
-            }
-        } else if (lineStructure[k] == 'jj') {
-            word = chooseNoun();
-        } else if (/rb.*/.test(lineStructure[k])){
-            word = chooseAdverb();
+            lineByToken.push(word);
+        }
+    line = RiTa.untokenize(lineByToken);
+    console.log(line);
+    } else {
+        if (exampleLines.length > 1){
+            let randomLine = Math.floor(Math.random() * exampleLines.length);
+            line = exampleLines[randomLine];
+            exampleLines.splice[randomLine];
         } else {
-            word = ''
+            line = '';
         }
-        if (k > 0 && word == lineByToken[k-1]){
-            word = ''
-        }
-        lineByToken.push(word);
+        
     }
-    let line = RiTa.untokenize(lineByToken);
     return line;
 }
 
@@ -273,7 +383,7 @@ function chooseNoun(){
 async function findSynonyms(word){
     try {
         var results = await fetch(
-            `https://api.wordnik.com/v4/word.json/${word}/relatedWords?useCanonical=true&relationshipTypes=synonym&limitPerRelationshipType=30&api_key=lfsv7wu4eq7pm2ymapwybd9z9dxq0u127siggnjw6i7te6i3u`
+            `https://api.wordnik.com/v4/word.json/${word}/relatedWords?useCanonical=true&relationshipTypes=synonym&limitPerRelationshipType=50&api_key=lfsv7wu4eq7pm2ymapwybd9z9dxq0u127siggnjw6i7te6i3u`
         )
     
         let Wordnik = await results.json();
@@ -293,7 +403,7 @@ async function findSynonyms(word){
 async function findSameContext(word){
     try {
         var results = await fetch(
-            `https://api.wordnik.com/v4/word.json/${word}/relatedWords?useCanonical=true&relationshipTypes=same-context&limitPerRelationshipType=30&api_key=lfsv7wu4eq7pm2ymapwybd9z9dxq0u127siggnjw6i7te6i3u`
+            `https://api.wordnik.com/v4/word.json/${word}/relatedWords?useCanonical=true&relationshipTypes=same-context&limitPerRelationshipType=50&api_key=lfsv7wu4eq7pm2ymapwybd9z9dxq0u127siggnjw6i7te6i3u`
         )
     
         let Wordnik = await results.json();
@@ -310,7 +420,7 @@ async function findSameContext(word){
 async function findEquivalent(word){
     try {
         var results = await fetch(
-            `https://api.wordnik.com/v4/word.json/${word}/relatedWords?useCanonical=true&relationshipTypes=equivalent&limitPerRelationshipType=30&api_key=lfsv7wu4eq7pm2ymapwybd9z9dxq0u127siggnjw6i7te6i3u`
+            `https://api.wordnik.com/v4/word.json/${word}/relatedWords?useCanonical=true&relationshipTypes=equivalent&limitPerRelationshipType=50&api_key=lfsv7wu4eq7pm2ymapwybd9z9dxq0u127siggnjw6i7te6i3u`
         )
     
         let Wordnik = await results.json();
@@ -330,65 +440,6 @@ function startsWithCapital(word){
 
 function lineByPOS(){
     // lines from my poetry! A lot of different lines to help add variety
-    let exampleLines = [
-    'sometimes I dream of a new place,',
-    'a place where all the things', 
-    'we hope for are true',
-    'we are there',
-    'floating,',
-    'breathing, hoping and',
-    'longing',
-    'for a new time, a new place',
-    'and yet here we stand in our current one,', 
-    'lopsided and',
-    'saddened by the way that we',
-    'allow ourselves to destroy',
-    'all that is around us',
-    'destroying in the name of creating',
-    'a creating that is lost at heart',
-    'and overshadowed by our own selfish gain',
-    'tomorrow we will awaken from this',
-    'terrible long slumber and realize that we have',
-    'burnt our hopes to the ground in the',
-    'dream of making our life a living heaven',
-    'and yet we have made it a hell',
-    'Lighting the night sky',
-    'Creating the starts mother',
-    'Disappears at dawn',
-    'Death sounds like sobbing',
-    'Death looks like a wilted flower in April',
-    'Orchid',
-    'Blooming and light',
-    'Tropical perfection',
-    'Paradise in a world of hate',
-    'Lovely',
-    'I never learnt how to count young',
-    'In my dreams I am a ballerina',
-    'I am graceful like a swan,',
-    'Twirling, Leaping, Soaring',
-    'Beautiful and Glamorous',
-    'Would I be somewhere different if I',
-    'actually had listened',
-    'to myself when I spoke',
-    'in my head about',
-    'the ways that I want to be',
-    'in this world, the shapes that I',
-    'want to manifest',
-    'and take up space in these shapes',
-    'that have rigids and soft',
-    'edges - rigid and soft',
-    'around the edges was',
-    'how I hoped to be and yet',
-    'here I am wandering in',
-    'a fog of my own creation',
-    'mapless, crying on',
-    'the path that I carved',
-    'out of the lies',
-    'I told myself in the hopes',
-    'that others would think',
-    'that I was soft and rigid',
-    'like a knife in the back',
-    'of a pillow'];
     for (let i = 0; i < exampleLines.length; i++){
         let tokenized = RiTa.tokenize(exampleLines[i]);
         let pos = RiTa.pos(tokenized);
